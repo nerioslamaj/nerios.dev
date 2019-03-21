9abccc58a8002348e4430a8cac1fb61708df88f0
nerioslamaj <nerios.lamaj@gmail.com>
2019-03-21 00:17:16 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Shader from './components/shader/Shader.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Shader></Shader>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
