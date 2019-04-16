43dfc329887b89aa9c66725068715178d35599d5
nerioslamaj <nerios.lamaj@gmail.com>
2019-04-16 12:06:58 +0100

    Changing the book

--- src/components/MeNowModal/MeNowModal.jsx
+++ src/components/MeNowModal/MeNowModal.jsx
@@ -23,7 +23,7 @@ class MeNowModal extends Component {
           <h1 className="alt">Recent Update</h1>
           <h6>11th of April 2019</h6>
           <p>Studying M.Sc. Computer Science at London MET and working as a Frontend Engineer at <a onClick={(e) => this.modalLinkClick(e.target.textContent)} href="https://www.thortful.com/">Thortful Ltd</a>. Also I am working on my master thesis which consists of a thorough research and application of Machine Learning algorithms in Javascript.</p>
-          <p>Currently reading <a onClick={(e) => this.modalLinkClick(e.target.textContent)} href="https://www.goodreads.com/book/show/8520610-quiet">"Quiet"</a> by Susan Cain.</p>
+          <p>Currently reading <a onClick={(e) => this.modalLinkClick(e.target.textContent)} href="https://www.goodreads.com/book/show/38820046-21-lessons-for-the-21st-century">"21 Lessons for the 21st Century"</a> by Yuval Noah Harari.</p>
           <Button btnColor={'#f74f3f'}  btnText={'Close'} btnClick={'CLOSE_MODAL'}></Button>
         </div>
       </TouchScrollable>
