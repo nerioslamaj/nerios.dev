6e33c0293205ca5988b12cb1da3f920e4da81465
nerioslamaj <nerios.lamaj@gmail.com>
2019-04-20 13:07:20 +0100

    Configuring the react route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
@@ -13,7 +14,14 @@ const store = createStore(rootReducer);
 ReactGA.initialize('UA-102725917-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
